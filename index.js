8516d45589ef0dd11feb0e0282f646a17964438a
Harry Shen <harshen@cisco.com>
2019-12-03 15:20:53 +1100

    submission

--- index.js
+++ index.js
@@ -2,34 +2,59 @@ const fs = require("fs");
 const axios = require("axios");
 const util = require("util");
 const inquirer = require("inquirer");
+const generateHTML = require("./generateHTML.js");
+const puppeteer = require("puppeteer");
 
-const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
 
-async function generateResume(){
-    const { gitUserName, backgroundColor }= await inquirer.prompt([
-        {
-            type:"input",
-            name:"gitUserName",
-            location:"Please input your GitHub Username"
-        },
-        {
-            type:"list",
-            name: "backgroundColor",
-            message: "Please choose your background color",
-            choices:[
-                "orange",
-                "purple",
-                "green",
-                "red",
-                "amber",
-                "navy blue",
-                "space grey"
-            ]
-        }
-    ])
-    console.log(`{${gitUserName},${backgroundColor}}`);
+async function write2PDF(html, outputFile){
+
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.setContent(html,{waitUntil:"networkidle0"});
+    await page.pdf({
+        path:outputFile,
+        format: `A4`,
+        printBackground:true
+    });
+    await browser.close();
+    process.exit();
 }
 
+ 
+async function generateResume(){
+    try{
+        const { gitUserName, backgroundColor }= await inquirer.prompt([
+            {
+                type:"input",
+                name:"gitUserName",
+                location:"Please input your GitHub Username"
+            },
+            {
+                type:"list",
+                name: "backgroundColor",
+                message: "Please choose your background color",
+                choices:[
+                    "blue",
+                    "green",
+                    "pink",
+                    "red"
+                ]
+            }
+        ])
+        const userQueryUrl = `https://api.github.com/users/${gitUserName}`;
+        const repoQueryUrl = `https://api.github.com/users/${gitUserName}/repos`;
+        const userResponse = await axios.get(userQueryUrl);
+        const repoResponse = await axios.get(repoQueryUrl);
+        const starArray = repoResponse.data.map(repo => parseInt(repo.watchers));
+        const locationURI = encodeURI(`https://www.google.com/maps/place/${userResponse.data.location}`)
+        const totalStars = starArray.reduce((a,b)=> a+b,0);
+        const resultHTML = generateHTML.generateHTMLHead(backgroundColor)+generateHTML.generateHTMLBody(userResponse.data,totalStars, locationURI);
+        await writeFileAsync(`${gitUserName}.html`, resultHTML);
+        const pdfRes = await write2PDF(resultHTML,`${gitUserName}.pdf`);
+    }catch(err){
+        console.log(err);
+    }
+}
 
 generateResume();
